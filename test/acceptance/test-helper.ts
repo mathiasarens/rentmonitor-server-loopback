9c7761599d1e91240a0f27bf990f1cd96d66adff
Mathias Arens <mathias.arens@googlemail.com>
2019-01-31 12:50:51 -0800

    Circle CI: Next try to make the tests run in an docker environment.

--- test/acceptance/test-helper.ts
+++ test/acceptance/test-helper.ts
@@ -6,8 +6,11 @@ import {
 } from '@loopback/testlab';
 
 export async function setupApplication(): Promise<AppWithClient> {
+  const config = givenHttpServerConfig();
+  config.host = '127.0.0.1';
   const app = new RentmonitorServerApplication({
-    rest: givenHttpServerConfig(),
+    //rest: givenHttpServerConfig(),
+    rest: config,
   });
 
   await app.boot();
