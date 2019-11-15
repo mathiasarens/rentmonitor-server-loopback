e7968f805c1212106aaef7c8a5cc43bf3af7c579
Mathias Arens <mathiasarens@Mathiass-MacBook-Pro.local>
2019-11-15 00:06:02 +0000

    Added one more test for UserController.findById()

--- src/__tests__/acceptance/user.controller.acceptance.ts
+++ src/__tests__/acceptance/user.controller.acceptance.ts
@@ -1,13 +1,13 @@
-import { DataObject } from '@loopback/repository';
-import { UserProfile } from '@loopback/security';
-import { Client, expect } from '@loopback/testlab';
-import { RentmonitorServerApplication } from '../..';
-import { PasswordHasherBindings } from '../../keys';
-import { User } from '../../models';
-import { ClientRepository, UserRepository } from '../../repositories';
-import { PasswordHasher } from '../../services/authentication/hash.password.bcryptjs';
-import { JWTService } from '../../services/authentication/jwt.service';
-import { givenEmptyDatabase, setupApplication } from './test-helper';
+import {DataObject} from '@loopback/repository';
+import {UserProfile} from '@loopback/security';
+import {Client, expect} from '@loopback/testlab';
+import {RentmonitorServerApplication} from '../..';
+import {PasswordHasherBindings} from '../../keys';
+import {User} from '../../models';
+import {ClientRepository, UserRepository} from '../../repositories';
+import {PasswordHasher} from '../../services/authentication/hash.password.bcryptjs';
+import {JWTService} from '../../services/authentication/jwt.service';
+import {givenEmptyDatabase, setupApplication} from './test-helper';
 
 describe('UserController Acceptence Test', () => {
   let app: RentmonitorServerApplication;
@@ -15,7 +15,7 @@ describe('UserController Acceptence Test', () => {
   let jwtService: JWTService;
 
   before('setupApplication', async () => {
-    ({ app, client: http, jwtService } = await setupApplication());
+    ({app, client: http, jwtService} = await setupApplication());
   });
   beforeEach(clearDatabase);
 
@@ -23,6 +23,7 @@ describe('UserController Acceptence Test', () => {
     await app.stop();
   });
 
+  // login
   it('should return jwt token on successful login', async () => {
     const clientId = await setupClientInDb('TestClient1');
     const testUser = getTestUser('1');
@@ -30,7 +31,7 @@ describe('UserController Acceptence Test', () => {
     const res = await http
       .post('/users/login')
       .set('Content-Type', 'application/json')
-      .send({ email: testUser.email, password: testUser.password });
+      .send({email: testUser.email, password: testUser.password});
 
     expect(res.status).to.eql(200);
     const userProfile = await decryptJWTToken(res.body.token);
@@ -45,13 +46,14 @@ describe('UserController Acceptence Test', () => {
     const res = await http
       .post('/users/login')
       .set('Content-Type', 'application/json')
-      .send({ email: testUser.email, password: '1' });
+      .send({email: testUser.email, password: '1'});
 
     expect(res.status).to.eql(401);
     expect(res.body.error.message).to.eql('Invalid email or password.');
   });
 
-  // TODO Tests for findById etc.
+  // findById
+
   it('should not find users from other clients', async () => {
     const clientId1 = await setupClientInDb('TestClient1');
     const clientId2 = await setupClientInDb('TestClient2');
@@ -62,7 +64,7 @@ describe('UserController Acceptence Test', () => {
     const loginResponse = await http
       .post('/users/login')
       .set('Content-Type', 'application/json')
-      .send({ email: testUser1.email, password: testUser1.password });
+      .send({email: testUser1.email, password: testUser1.password});
 
     expect(loginResponse.status).to.eql(200);
 
@@ -73,6 +75,32 @@ describe('UserController Acceptence Test', () => {
 
     expect(response.status).to.eql(200);
     expect(response.body).to.be.null();
+  });
+
+  it('should find user from the same client', async () => {
+    const clientId1 = await setupClientInDb('TestClient1');
+    const testUser1 = getTestUser('1');
+    const testUser2 = getTestUser('2');
+    await setupUserInDb(clientId1, testUser1);
+    const dbUser2 = await setupUserInDb(clientId1, testUser2);
+    const loginResponse = await http
+      .post('/users/login')
+      .set('Content-Type', 'application/json')
+      .send({email: testUser1.email, password: testUser1.password});
+
+    expect(loginResponse.status).to.eql(200);
+
+    const response = await http
+      .get('/users/' + dbUser2.id)
+      .set('Authorization', 'Bearer ' + loginResponse.body.token)
+      .set('Accept', 'application/json');
+
+    expect(response.status).to.eql(200);
+    expect(response.body.clientId).to.eql(clientId1);
+    expect(response.body.firstName).to.eql(dbUser2.firstName);
+    expect(response.body.lastName).to.eql(dbUser2.lastName);
+    expect(response.body.email).to.eql(dbUser2.email);
+    expect(response.body.id).to.eql(dbUser2.id);
   });
 
   // non test methods --------------------------------------------------------------------
@@ -87,7 +115,7 @@ describe('UserController Acceptence Test', () => {
 
   async function setupClientInDb(name: string): Promise<number> {
     const clientRepository = await app.getRepository(ClientRepository);
-    const clientFromDb = await clientRepository.create({ name: name });
+    const clientFromDb = await clientRepository.create({name: name});
     return clientFromDb.id;
   }
 
