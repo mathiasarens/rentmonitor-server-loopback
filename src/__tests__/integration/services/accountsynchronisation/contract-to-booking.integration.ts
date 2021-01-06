717add356204f1d2f0ceac9390e6a3084ff588d0
Mathias Arens <mathias@arens-festers.de>
2021-01-06 23:53:01 +0100

    More tests for contract-to-booking service

--- src/__tests__/integration/services/accountsynchronisation/contract-to-booking.integration.ts
+++ src/__tests__/integration/services/accountsynchronisation/contract-to-booking.integration.ts
@@ -194,7 +194,7 @@ describe('Contract To Booking Service Integration Tests', () => {
     expect(savedBookings[0].accountTransactionId).to.eql(null);
   });
 
-  it('should create two new booking', async function () {
+  it('should create two new bookings', async function () {
     // given
     const client = await clientRepository.create({
       name: 'Contract to booking integration test',
@@ -251,5 +251,59 @@ describe('Contract To Booking Service Integration Tests', () => {
     expect(savedBookings[1].amount).to.eql(-1 * unsavedContract1.amount);
     expect(savedBookings[1].contractId).to.eql(savedContract1.id);
     expect(savedBookings[1].accountTransactionId).to.eql(null);
+  });
+
+  it('should not create a booking on the last day of the contract', async function () {
+    // given
+    const client = await clientRepository.create({
+      name: 'Contract to booking integration test',
+    });
+
+    const tenant1 = new Tenant({
+      clientId: client.id,
+      name: 'Tenant 1',
+      accountSynchronisationName: 'Tenant1',
+    });
+    const savedTenant1 = await tenantRepository.create(tenant1);
+
+    const startDate = new Date(2019, 3, 10);
+    const endDate = new Date(2019, 4, 10);
+    const unsavedContract1 = new Contract({
+      clientId: client.id,
+      tenantId: savedTenant1.id,
+      start: startDate,
+      end: endDate,
+      rentDueDayOfMonth: 10,
+      rentDueEveryMonth: 1,
+      amount: 1000,
+    });
+    const savedContract1 = await contractRepository.create(unsavedContract1);
+
+    // when
+    const {
+      newBookings,
+      matchedContracts,
+      unmatchedContracts,
+    } = await contractToBookingService.createAndSaveBookingsForContracts(
+      new Date(2019, 5, 15),
+      client.id,
+    );
+
+    // than
+    expect(newBookings).to.eql(1);
+    expect(matchedContracts).to.eql(1);
+    expect(unmatchedContracts).to.eql(0);
+
+    const savedBookings: Booking[] = await bookingRepository.find({
+      where: {clientId: client.id},
+      order: ['date ASC'],
+    });
+    expect(savedBookings).length(1);
+    expect(savedBookings[0].date).to.eql(new Date(2019, 3, 10));
+    expect(savedBookings[0].tenantId).to.eql(savedTenant1.id);
+    expect(savedBookings[0].comment).to.eql('Miete 3/2019');
+    expect(savedBookings[0].amount).to.eql(-1 * unsavedContract1.amount);
+    expect(savedBookings[0].contractId).to.eql(savedContract1.id);
+    expect(savedBookings[0].accountTransactionId).to.eql(null);
   });
 });
