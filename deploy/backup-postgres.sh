652e558384bfe28a0692f96cfbcf25cd2b727948
Mathias Arens <mathias@arens-festers.de>
2021-03-25 22:44:21 +0100

    Updated scripts

--- deploy/backup-postgres.sh
+++ deploy/backup-postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NOW=`date +%d-%m-%Y"_"%H_%M_%S`
-docker exec -t $BACKUP_DB pg_dumpall -c -U $BACKUP_DB_USER > backup/rentmonitor-$NOW.sql
+docker exec -t $BACKUP_DB pg_dump -c -U $BACKUP_DB_USER rentmonitor > ~/backup/rentmonitor-$NOW.sql
 scp -i ~/.ssh/$BACKUP_HOST ~/backup/rentmonitor-$NOW.sql $BACKUP_HOST:~
