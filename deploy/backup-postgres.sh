94b7257cb6d30e7642b39193c89824dce3381a57
Mathias Arens <mathias@arens-festers.de>
2021-03-29 00:28:27 +0200

    Modified backup file name

--- deploy/backup-postgres.sh
+++ deploy/backup-postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NOW=`date +%d-%m-%Y"_"%H_%M_%S`
+NOW=`date +%Y-%m-%d"_"%H_%M_%S`
 docker exec -t $BACKUP_DB pg_dump -c -U $BACKUP_DB_USER rentmonitor > ~/backup/rentmonitor-$NOW.sql
 scp -i ~/.ssh/$BACKUP_HOST ~/backup/rentmonitor-$NOW.sql $BACKUP_HOST:~
