3d224911309f540525709baa27acc1f1ccbb7627
Mathias Arens <mathias@arens-festers.de>
2021-10-01 23:10:09 +0200

    Update deploy-aws.sh

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -4,6 +4,6 @@ VERSION=$(npx -c 'echo "$npm_package_version"')
 echo $VERSION
 docker build -t amd64/rentmonitor-server-loopback:$VERSION .
 docker save -o /tmp/rentmonitor-server-loopback-$VERSION.img amd64/rentmonitor-server-loopback:$VERSION
-
+eb deploy
 DURATION=$SECONDS
 echo "rentmonitor-server:$VERSION deployment took $(($DURATION / 60)) minutes and $(($DURATION % 60)) seconds."
