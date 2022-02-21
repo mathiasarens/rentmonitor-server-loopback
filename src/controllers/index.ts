82fe5aa5a72101737bf9dd75a653b477c3639544
Mathias Arens <mathias@arens-festers.de>
2022-02-21 05:40:38 +0100

    Added back ping controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,5 +3,6 @@ export * from './account-transaction.controller';
 export * from './booking.controller';
 export * from './client.controller';
 export * from './contract.controller';
+export * from './ping.controller';
 export * from './tenant.controller';
 export * from './version.controller';
