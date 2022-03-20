78c09b9a4f8fa5d031d21b72059714d13b6ffae7
Mathias Arens <mathias@arens-festers.de>
2022-03-20 05:38:48 +0100

    eslintrc.js update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: '@loopback/eslint-config',
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+      },
+    },
+  ],
+  rules: {
+    'mocha/handle-done-callback': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+  },
+  parserOptions: {
+    createDefaultProgram: true,
+  },
 };
