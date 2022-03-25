8e356f9360dce9b66735358a432c372ed4e2a08e
Mathias Arens <mathias@arens-festers.de>
2022-03-25 03:57:59 +0100

    Added cors response header

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,14 @@ if (require.main === module) {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
       },
+      cors: {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        maxAge: 86400,
+        credentials: true,
+      },
     },
   };
   main(config).catch(err => {
